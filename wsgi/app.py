bb585dbb06ccda1e353f0fa24bcf72b486a6c04c
Jonas Svatos <jonas.svatos@etnetera.cz>
2014-06-10 19:33:03 +0200

    tidy up, refactor

--- wsgi/app.py
+++ wsgi/app.py
@@ -1,28 +1,36 @@
 import requests
 import bs4
 from flask import Flask
-from flask import request
-from flask import url_for
 from flask import render_template
-from flask import Response
 
 app = Flask(__name__)
 src = 'http://www.ceskatelevize.cz/ct24/zive-vysilani/'
+
+
+iframe_working = True
 
 @app.route('/')
 def root():
 	r = requests.get(src)
 	soup = bs4.BeautifulSoup(r.text)
+	global iframe_working
 	for link in soup.find_all('iframe'):
 		iframe_link = link.get('src')
 		if 'iFramePlayerCT24' in iframe_link:
-			url = 'http://www.ceskatelevize.cz' +  iframe_link
-        return render_template('index.tpl', url=url)
+			url = 'http://www.ceskatelevize.cz' + iframe_link
+			iframe_working = True
+		else:
+			url = 'about:blank'
+			iframe_working = False
+		return render_template('index.html', url=url)
 
-@app.route('/static')
-def foo():
-        url_for('static', filename='.*')
+@app.route('/monitoring', methods=['GET'])
+def monitoring():
+	if iframe_working:
+		return 'blebleble OK'
+	else:
+		return 'blebleble CRITICAL'
 
 if __name__ == '__main__':
 	app.debug = True
-        app.run()
+	app.run('0.0.0.0')
