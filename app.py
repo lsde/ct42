0358d5c7ff63b2c6ad9ca632f37fabd60a50aef0
Jonas Svatos <jonas.svatos@etnetera.cz>
2016-05-23 16:26:36 +0200

    make it congurable

--- app.py
+++ app.py
@@ -4,14 +4,18 @@ from flask import render_template
 from selenium import webdriver
 
 app = Flask(__name__)
-
+app.debug = bool(int(os.getenv('APP_DEBUG', 0)))
+listen_host = os.getenv('LISTEN_HOST', 'localhost')
+listen_port = int(os.getenv('LISTEN_PORT', 5000))
+endpoint_host = os.getenv('ENDPOINT_HOST', 'localhost')
+endpoint_port = os.getenv('ENDPOINT_PORT', 8910)
 
 @app.route('/')
 def root():
     if 'Firefox' in str(request.user_agent):
         return render_template('unsupported.html')
 
-    driver = webdriver.Remote(command_executor="http://127.0.0.1:8910",
+    driver = webdriver.Remote(command_executor="http://{0}:{1}".format(endpoint_host, endpoint_port),
                               desired_capabilities={'javascriptEnabled' : True,
                                                     'loadImages' : False}
                               )
@@ -22,4 +26,4 @@ def root():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, threaded=True)
+    app.run(host=listen_host, port=listen_port, threaded=True)
