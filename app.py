85ecf30eab3c53d7c7dc90c19b58911fb4e3529e
Jonas Svatos <jonas.svatos@nfa.cz>
2019-11-29 22:46:42 +0100

    enable urls from ivysilani

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import os
+import traceback
 from flask import Flask, request, redirect
 from flask import render_template
 from selenium import webdriver
@@ -16,15 +17,30 @@ listen_port = int(os.getenv('LISTEN_PORT', 5000))
 endpoint_host = os.getenv('ENDPOINT_HOST', 'localhost')
 endpoint_port = os.getenv('ENDPOINT_PORT', 8910)
 
+@app.route('/<path:subpath>')
+def porady(subpath):
+    return root(subpath)
+
 @app.route('/')
-def root():
+def root(subpath=None):
     driver = webdriver.Remote(command_executor="http://{0}:{1}".format(endpoint_host, endpoint_port),
                               desired_capabilities=DesiredCapabilities.CHROME)
-    driver.get('https://ct24.ceskatelevize.cz/#live')
+    if subpath is not None:
+        div_class = 'video-player'
+        driver.get('https://www.ceskatelevize.cz/' + subpath)
+        try:
+            element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'video-play-btn')))
+            element.click()
+        except Exception, err:
+            traceback.print_exc()
+    else:
+        div_class = 'live-video'
+        driver.get('https://ct24.ceskatelevize.cz/#live')
     try:
-        live_video = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "live-video")))
-        url = live_video.find_element_by_tag_name('iframe').get_attribute('src')
-    except:
+        video = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, div_class)))
+        url = video.find_element_by_tag_name('iframe').get_attribute('src')
+    except Exception, err:
+        traceback.print_exc()
         return redirect('/')
     finally:
         driver.quit()
